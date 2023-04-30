f35e86882de8c00afe86c9a1db0281398af2af67
Uchenna Chiagozie <96476199+officialp123@users.noreply.github.com>
2023-04-30 12:33:52 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,50 +1,17 @@
-class Character {
-  constructor(name, health, strength) {
-    this.name = name;
-    this.health = health;
-    this.strength = strength;
-    this.xp = 0; // XP is always zero for new characters
-    this.inventory = [10,1]; // Gold: inventory[0] and Keys: inventory[1]
+[
+  {
+    name: "Chiagozie Uchenna",
+    title: "Software Engineer",
+    description: "Chiagozie is a software developer with 1 year of experience in programming and handling projects. He is from Anambra State, Nigeria, he is a christian too, he studied in Paul University and loves to read and write code for students."
+  },
+  {
+    name: "Uchenna Samuel",
+    title: "Graphic Designer",
+    description: "Samuel is a graphic designer with 5 years of experience in designing graphics and web designs, he is too creative and lovely to work with, he is a nice team player also a little knowledge of leadership skills, he applied his creative designs in his workflows"
+  },
+  {
+    name: "Uchenna Chiamaka",
+    title: "Data Analyst",
+    description: "Chiamaka is a data analyst with 5 years of experience in designing data and web designs, he is too creative and lovely to work with, he is a nice team player also a little knowledge of leadership skills, he applied his creative designs in his workflows"
   }
-  // Attack a target
-  attack(target) {
-    if (this.health > 0) {
-      const damage = this.strength;
-      console.log(
-        `${this.name} attacks ${target.name} and causes ${damage} damage points`
-      );
-      target.health -= damage;
-      if (target.health > 0) {
-        console.log(`${target.name} has ${target.health} health points left`);
-      } else {
-        target.health = 0;
-        const bonusXP = 10;
-        const goldWon = target.inventory[0];
-        const keysWon = target.inventory[1];
-        console.log(
-          `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points
-          and ${goldWon} gold and ${keysWon} keys`
-        );
-        this.xp += bonusXP;
-        this.inventory[0] += goldWon;
-        this.inventory[1] += keysWon;
-      }
-    } else {
-      console.log(`${this.name} can't attack (they've been eliminated)`);
-    }
-  }
-  // Return the character description
-  describe() {
-    return `${this.name} has ${this.health} health points, ${this
-      .strength} as strength and ${this.xp} XP points and ${this.inventory[0]} gold 
-      and ${this.inventory[1]} keys`;
-  }
-}
-const aurora = new Character("Aurora", 150, 25);
-const glacius = new Character("Glacius", 130, 30);
-const monster = new Character("Spike", 40, 20);
-
-monster.attack(aurora);
-monster.attack(glacius);
-aurora.attack(monster);
-glacius.attack(monster);
+]
